66335c97f750683dc64b328ee625b4ed4d33699a
Liu Kai <liukai306@163.com>
2026-02-07 19:29:43 +0800

    添加user和message类

--- src/com/lucas/qqcommon/User.java
+++ src/com/lucas/qqcommon/User.java
@@ -1,5 +1,33 @@
 package com.lucas.qqcommon;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String userId;  // 用户id
+    private String passwd;  // 用户密码
+
+    public User() {}
+
+    public User(String userId, String passwd) {
+        this.userId = userId;
+        this.passwd = passwd;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
 }
